597c0d95744fb18bc2794b9272a759ef3b5b7cf9
nishantP-10 <nishantparekh@ymail.com>
2014-11-30 22:19:02 -0500

    Project made

---
+++ src/RudiExecutor.java
@@ -0,0 +1,77 @@
+
+public class RudiExecutor {
+
+	
+	//checks for the string to be an Integer or not
+	@SuppressWarnings("unused")
+	private boolean checkInt(String s){
+		
+		try{
+			
+			Integer.parseInt(s);
+			
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+		return true;
+	}
+	
+	//checks for the string to be a Float or not.
+	@SuppressWarnings("unused")
+	private boolean checkFloat(String s){
+		
+		try{
+			
+			Float.parseFloat(s);
+			
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+		return true;
+	}
+	
+	//Checks if entered string is either integer or float. 
+	@SuppressWarnings("unused")
+	private boolean checkIntegerOrFloat(String s){
+		if(checkFloat(s)){
+			return true;
+			
+		}else if(checkInt(s)){
+		return true;	
+		}	
+		else
+		return false;
+	}
+	
+	private boolean assignment(String s){
+		if(s.contains("=")){
+		String parts[]=s.split("=");
+		//checkMap()
+		
+		return true;
+		
+		}
+		else
+		return false;
+	}
+	
+	private boolean add(String s){
+		if(s.contains("+")){
+		String parts[]=s.split("+");
+		//
+		//checkValues(s[0]);
+		return true;
+		
+		}
+		else
+		return false;
+	}
+	
+	
+	
+	
+}
+
+	
