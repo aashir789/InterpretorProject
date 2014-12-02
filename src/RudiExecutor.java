4883e888805703be4a81d1ede6dbc8b01808d26c
nishantP-10 <nishantparekh@ymail.com>
2014-12-02 08:03:39 -0500

    Updated

--- src/RudiExecutor.java
+++ src/RudiExecutor.java
@@ -4,11 +4,13 @@ import java.util.regex.Pattern;
 
 public class RudiExecutor {
 
-	public static String execute(Map<String, String> localVariableTypes,
+	public String execute(Map<String, String> localVariableTypes,
 			Map<String, String> localVariableValues,
 			ArrayList<String> programInstructionList) {
+		String vals[];
+		boolean bool=false;
 		for (String s : programInstructionList) {
-
+		
 			switch (s) {
 			/*
 			 * keywords
@@ -26,17 +28,24 @@ public class RudiExecutor {
 			/*
 			 * Arithmetic =,+,-,/,*
 			 */
+			
 			case "-":
-
 			case "+":
-
 			case "*":
-
 			case "/":
-
+				s=PlaceVariableValues(s);
 				float answer = EvaluateExpression(s);
+				break;	
+		  
+			case ":eq:":
+			case ":ne:":
+			case ":gt:":
+			case ":lt:":
+			case ":ge:":
+			case ":le:":
+				s=PlaceVariableValues(s);
+				boolean result= EvaluateLogicalExpression(s);
 				break;
-
 			}
 
 			/*
@@ -54,82 +63,106 @@ public class RudiExecutor {
 			 * 
 			 * input variable
 			 */
-
+			//case "input ":
+				
+					
 		}
 		return null;
 	}
 
-	// 1-2+3*4/6
-	
-	private float DoCalculations(String s){
-		float answer=0;
-		String brackexp="";
-		
-		while(s.contains("(")){
-			
-			while(brackexp.contains("(")){
-				int startIndex=0,brackoccur=0,endbrack=0;
-				if(s.contains("("))
-				brackoccur=s.indexOf("(",startIndex);
-				endbrack=s.indexOf(")",brackoccur);
-				brackexp=s.substring(brackoccur,endbrack);
-		}
-			//brackexp.
-		}
-		return 1;
-	}
-	
-	
-
-	private static float EvaluateExpression(String s) {
+	private boolean EvaluateLogicalExpression(String s) {
+		return false;
 		// TODO Auto-generated method stub
+		
+		
+	}
+
+	// 1-2+3*4/6
+/*	
+	*//**
+	* Returns the last position of c outside of all brackets in the string s.
+	* @param s the string in which is searched for
+	* @param c the character to find
+	* @return the index of the first occurrence of the character
+	*/
+	private static int findIndex(String s, char c)
+	{
+	  int count = 0;
+	  // searching the string from back to front
+	  for (int i = s.length() - 1; i >= 0; i--)
+	  {
+	    if (s.charAt(i) == '(') count++; // count openening brackets
+	    if (s.charAt(i) == ')') count--; // count closing brackets
+	    // count = 0 means we are outside all brackets
+	    if (s.charAt(i) == c && count == 0)
+	      // found the rightmost occurence outside all brackets
+	      return i;
+	  }
+	  return -1;
+	}	
+	
+private static float EvaluateExpression(String s) {
+	
+	// TODO Auto-generated method stub
+		s=PlaceVariableValues(s);
 		float val = 0;
-		String parts[];		
-		while (true) {
-			if (s.contains("-")) {
-				parts = s.split("-");
-				if (parts.length == 2) {
-					val = EvaluateExpression(parts[0])
-							- EvaluateExpression(parts[1]);
-					break;
-				}
-			} else {
-				if (s.contains("+")) {
-					parts = s.split(Pattern.quote("+"));
-					if (parts.length == 2) {
-						val = EvaluateExpression(parts[0])
-								+ EvaluateExpression(parts[1]);
-						break;
-					}
-				} else {
-					if (s.contains("*")) {
-						parts = s.split(Pattern.quote("*"));
-						if (parts.length == 2) {
-							val = EvaluateExpression(parts[0])
-									* EvaluateExpression(parts[1]);
-							break;
-						}
-					} else {
-						if (s.contains("/")) {
-							parts = s.split("/");
-							if (parts.length == 2)
-								val = EvaluateExpression(parts[0])
-										/ EvaluateExpression(parts[1]);
-							break;
-						} else {
-							
-							
+		int index=0;
+		//String parts[];	
+		
+			if ((index = (findIndex(s, '-'))) >= 0) {
+					return (EvaluateExpression(s.substring(0,index))
+							- EvaluateExpression(s.substring(index+1, s.length())));
+			} else
+				if ((index = (findIndex(s, '+'))) >= 0) {
+					return (EvaluateExpression(s.substring(0,index))
+							+ EvaluateExpression(s.substring(index+1, s.length())));
+				} else
+					if ((index = (findIndex(s, '*'))) >= 0) {
+						return (EvaluateExpression(s.substring(0,index))
+								* EvaluateExpression(s.substring(index+1, s.length())));
+					} else
+						if ((index = (findIndex(s, '/'))) >= 0) {
+							return (EvaluateExpression(s.substring(0,index))
+									/ EvaluateExpression(s.substring(index+1, s.length())));
+						} /*
+						
+						else {
+			
 							//Add map details here
-							
-							val = Float.parseFloat(s);
-							break;
+							return Float.parseFloat(s);
 						}
-					}
-				}
-			}
-		}
-		// System.out.println("The answer is"+val);
+				//}
+		//	}
+	//	}
+			
+*/			
+			if (s.charAt(0) == '(')
+			  {
+			    if (s.charAt(s.length()-1) == ')')
+			      return (EvaluateExpression(s.substring(1, s.length()-1)));
+			    //else
+			      //throw new ParserException("Invalid brackets: " + s);
+			  }
+
+			  // now finally convert the string (that hopefully consists of number) to a double type.
+			  try {
+			    return Float.parseFloat(s);
+			  } catch (NumberFormatException ex) {
+			    //throw new ParserException("String to number parsing exception: " + s);
+			  }	
 		return val;
+	}
+
+	
+	
+	
+	
+	private static String PlaceVariableValues(String s) {
+	// TODO Auto-generated method stub
+	String valSplit[]=s.split(" ");
+	for(String ch:valSplit){
+						}		
+return "";
 	}
 
 	// checks for the string to be an Integer or not
@@ -195,7 +228,7 @@ public class RudiExecutor {
 	}
 
 	public static void main(String args[]) {
-		String s = "12+12-12/3*6";
+		String s = "(1-2)+3*(4/8)";
 		Float result = 0f;
 		result = EvaluateExpression(s);
 		System.out.println("Answer is: " + result);
